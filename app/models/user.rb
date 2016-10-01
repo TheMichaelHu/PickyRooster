6be1a31d9c3aff0eee5e97b094c60297acfb3e88
Michael <michaelanhu@gmail.com>
2016-10-01 16:46:44 -0400

    added friendship system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :friend_requests, dependent: :destroy
+  has_many :pending_friends, through: :friend_requests, source: :friend
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def remove_friend(friend)
+    current_user.friends.destroy(friend)
+  end
 end
