a587986af96ceb57a3a3df9e4f4df9544856ae29
Michael <michaelanhu@gmail.com>
2016-10-02 10:24:03 -0400

    bug fixes

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,7 +2,7 @@ class FriendsController < ApplicationController
   before_action :set_friend, only: :destroy
 
   def index
-    @friends = current_user.friends(&:email)
+    @friends = current_user.friends
   end
 
   def show
@@ -18,9 +18,9 @@ class FriendsController < ApplicationController
       @users = User.all
 
       if params[:search]
-          @users = User.search(params[:search]).map(&:email)
+          @users = User.search(params[:search])
       else
-          @users = User.all.map(&:email)
+          @users = User.all
       end
   end
 
